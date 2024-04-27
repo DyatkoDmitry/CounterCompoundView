66b803612b957d5250c55a42a7a2067d60789ddc
Dyatko Dmitry <DyatkoDmitry@gmail.com>
2024-04-27 17:54:56 +0300

    feat: added UpdateCounterDialogFragment

--- app/src/main/java/com/example/countercompoundview/Counter.kt
+++ app/src/main/java/com/example/countercompoundview/Counter.kt
@@ -1,9 +1,14 @@
 package com.example.countercompoundview
 
 import android.content.Context
+import android.graphics.drawable.PaintDrawable
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.google.android.material.button.MaterialButton
 
 class Counter @JvmOverloads constructor(
@@ -12,13 +17,34 @@ class Counter @JvmOverloads constructor(
 
     private var corner:Int = 0
 
+    private val onClickTap: () -> Unit = {
+        addCorner(CONST.EXTRA)
+    }
+
+    private val updateCounterDialogFragment = UpdateCounterDialogFragment(onClickTap)
+
     init{
         LayoutInflater.from(context).inflate(R.layout.counter_compound_view, this, true)
+        setButtonListener()
     }
 
     private fun setButtonListener(){
         findViewById<MaterialButton>(R.id.tapButton).setOnClickListener {
-
+            updateCounterDialogFragment.show((context as MainActivity).supportFragmentManager, CONST.UPDATE_COUNTER);
         }
     }
+
+    private fun addCorner(extra:Int){
+        corner+=extra
+        findViewById<TextView>(R.id.corner).text = corner.toString()
+
+        val paintDrawable = PaintDrawable(ContextCompat.getColor(context, R.color.yellow))
+        paintDrawable.setCornerRadius(corner.toFloat())
+        findViewById<View>(R.id.square).background = paintDrawable
+    }
+}
+
+object CONST{
+    const val EXTRA = 10
+    const val UPDATE_COUNTER = "UpdateCounterDialogFragment"
 }
--- app/src/main/java/com/example/countercompoundview/UpdateCounterDialogFragment.kt
+++ app/src/main/java/com/example/countercompoundview/UpdateCounterDialogFragment.kt
@@ -1,7 +1,16 @@
 package com.example.countercompoundview
 
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 
-class UpdateCounterDialogFragment(val onClick: () -> Unit): DialogFragment() {
+class UpdateCounterDialogFragment(val onClickTap:() -> Unit): DialogFragment() {
 
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setTitle(R.string.title)
+            .setPositiveButton(getString(R.string.positive)) { _,_ -> onClickTap.invoke() }
+            .setNegativeButton(R.string.negative){_,_->}
+            .create()
 }
